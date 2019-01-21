0b3da8e76c96c461d84ae5bd225a4f14d1c092cc
wcgbg <wangchengu@gmail.com>
2019-01-21 14:01:23 -0800

    Snake's food

--- snake/main.py
+++ snake/main.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-
+import time
+import random
 import curses
 import _curses
 
@@ -9,7 +10,8 @@ import _curses
 def main(stdscr):
     curses.curs_set(False)
     height, width = stdscr.getmaxyx()
-    snake = [(height // 2, width // 2)] * 3
+    snake = [(height // 2, width // 2)] * 5
+    food = (random.randint(0, height-1), random.randint(0, width-1))
     while True:
         # paint
         stdscr.clear()
@@ -22,6 +24,12 @@ def main(stdscr):
                 stdscr.addch(pos[0], pos[1], ch)
             except _curses.error as e:
                 pass
+        try:
+            stdscr.addch(food[0], food[1], '$')
+        except _curses.error as e:
+            pass
+        stdscr.refresh()
+        #time.sleep(1)
         # get input
         c = stdscr.getch()
         delta = None
@@ -46,6 +54,9 @@ def main(stdscr):
         for i in range(len(snake) - 1, 0, -1):
             snake[i] = snake[i - 1]
         snake[0] = (new_row, new_col)
+        if snake[0] == food:
+            food = (random.randint(0, height-1), random.randint(0, width-1))
+            snake.append(snake[-1])
 
 
 if __name__ == '__main__':

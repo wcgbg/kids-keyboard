840642185316b504b96d587d75bee69246b8b468
wcgbg <wangchengu@gmail.com>
2019-08-26 19:52:55 -0700

    color snake

--- snake/main.py
+++ snake/main.py
@@ -12,20 +12,26 @@ def main(stdscr):
     height, width = stdscr.getmaxyx()
     snake = [(height // 2, width // 2)] * 5
     food = (random.randint(0, height - 1), random.randint(0, width - 1))
+    head_color = 1
+    body_color = 2
+    food_color = 3
+    curses.init_pair(head_color, curses.COLOR_BLUE, curses.COLOR_BLACK)
+    curses.init_pair(body_color, curses.COLOR_BLUE, curses.COLOR_BLACK)
+    curses.init_pair(food_color, curses.COLOR_YELLOW, curses.COLOR_BLACK)
     while True:
         # paint
         stdscr.clear()
         for i, pos in reversed(list(enumerate(snake))):
-            if i == 0:
-                ch = '@'
-            else:
-                ch = 'o'
+            # TODO: try unicode '⬤⚫●'
             try:
-                stdscr.addch(pos[0], pos[1], ch)
+                if i == 0:
+                    stdscr.addch(pos[0], pos[1], '@', curses.A_BOLD | curses.color_pair(head_color))
+                else:
+                    stdscr.addch(pos[0], pos[1], 'o', curses.color_pair(body_color))
             except _curses.error as e:
                 pass
         try:
-            stdscr.addch(food[0], food[1], '$')
+            stdscr.addch(food[0], food[1], '$', curses.A_BLINK | curses.A_BOLD | curses.color_pair(food_color))
         except _curses.error as e:
             pass
         stdscr.refresh()
